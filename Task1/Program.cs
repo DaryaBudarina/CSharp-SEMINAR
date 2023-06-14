36a0020635aeca77b1226fb9b37977d1a0bfc09d
Darya_Budarina <budarina.d.e@mail.ru>
2023-06-14 11:07:40 +0600

    Семинар 1. Задание 1. Квадрат числа

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input number => ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int power = 2; // power of number
+int squareNumber = Convert.ToInt32(Math.Pow(number, power));
+// int squareNumber = (int)Math.Pow(number, power);
+Console.WriteLine($"Square {number} is {squareNumber}");
