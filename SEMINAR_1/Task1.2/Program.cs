72ad1f9d0cee9f2f5c6e3fe579814f905f1b81ab
Darya_Budarina <budarina.d.e@mail.ru>
2023-06-19 14:22:02 +0600

    add seminar 1

---
+++ SEMINAR_1/Task1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число 1 ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+if(a1 == a2 * a2)
+{
+    Console.Write($"Yes");
+}
+else
+{
+    Console.WriteLine($"No");
+}
