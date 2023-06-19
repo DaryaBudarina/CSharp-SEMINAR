94229aef071bdf5001a57a4d92f895db27b9b8fa
DaryaBudarina <135702026+DaryaBudarina@users.noreply.github.com>
2023-06-19 14:27:34 +0600

    Delete Task1 directory

--- Task1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Write("Input number => ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-int power = 2; // power of number
-int squareNumber = Convert.ToInt32(Math.Pow(number, power));
-// int squareNumber = (int)Math.Pow(number, power);
-Console.WriteLine($"Square {number} is {squareNumber}");
